8beddcb7b15543c6e6463258677929f657cfb03a
vstasch <vanessastasch1@hotmail.de>
2020-12-20 14:22:59 +0100

    exercise07.java

---
+++ exercise07.java
@@ -0,0 +1,48 @@
+package exercise07;
+
+import java.io.File;
+
+import java.io.IOException;
+import java.util.Random;
+
+import weka.classifiers.Evaluation;
+import weka.classifiers.trees.RandomForest;
+import weka.core.Instances;
+import weka.core.converters.ArffLoader;
+import weka.filters.Filter;
+import weka.filters.unsupervised.attribute.StringToWordVector;
+import weka.filters.unsupervised.attribute.NumericToNominal;
+
+
+public class Main {
+	public static void main(String[] args) throws IOException, Exception {
+
+		// load data
+		ArffLoader loader = new ArffLoader();
+		loader.setFile(new File("data.arff"));
+		Instances instances = loader.getDataSet();
+		instances.setClassIndex(instances.numAttributes() - 1);
+
+		// train classifier
+		RandomForest classifier = new RandomForest();
+		classifier.buildClassifier(instances);
+
+		Evaluation eval = new Evaluation(instances);
+		eval.crossValidateModel(classifier, instances, 10, new Random());
+	
+		//filter
+		StringToWordVector filter = new StringToWordVector(1000);
+		filter.attributeIndicesTipText();
+		
+		Instances filteredSet = Filter.useFilter(instances, filter);
+	
+		NumericToNominal ntnFilter = new NumericToNominal();
+		ntnFilter.setAttributeIndices("first");
+		ntnFilter.setInputFormat(filteredSet);
+		
+		Instances filteredSet1 = Filter.useFilter(filteredSet, ntnFilter);
+		
+		RandomForest classifiers = new RandomForest();
+		classifiers.buildClassifier(filteredSet1);
+	}	
+}
