1a9592c16cc3bc04250292896abf5b4b028822e8
Valeria Gavrilova <gavrivalery@gmail.com>
2020-12-29 20:09:30 +0300

    .DS_Store banished!

--- src/de/ukoeln/idh/teaching/tri/ex07/Main.java
+++ src/de/ukoeln/idh/teaching/tri/ex07/Main.java
@@ -1,28 +1,58 @@
-package de.ukoeln.idh.teaching.tri.ex07;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.Random;
+package com.vgavrilova;
 
 import weka.classifiers.Evaluation;
-import weka.classifiers.rules.OneR;
+import weka.classifiers.bayes.NaiveBayes;
 import weka.core.Instances;
 import weka.core.converters.ArffLoader;
+import weka.core.stemmers.SnowballStemmer;
+import weka.core.stopwords.Rainbow;
+import weka.core.tokenizers.WordTokenizer;
+import weka.filters.Filter;
+import weka.filters.unsupervised.attribute.NumericToNominal;
+import weka.filters.unsupervised.attribute.StringToWordVector;
+
+import java.io.File;
+import java.util.Random;
 
 public class Main {
-	public static void main(String[] args) throws IOException, Exception {
 
-		// load data
-		ArffLoader loader = new ArffLoader();
-		loader.setFile(new File("data.arff"));
-		Instances instances = loader.getDataSet();
-		instances.setClassIndex(instances.numAttributes() - 1);
+    public static void main(String[] args) throws Exception {
+        // load data
+        ArffLoader loader = new ArffLoader();
+        loader.setFile(new File("data/imdb.arff"));
+        Instances instances = loader.getDataSet();
+        instances.setClassIndex(instances.numAttributes() - 1);
 
-		// train classifier
-		OneR classifier = new OneR();
-		classifier.buildClassifier(instances);
+        // filter data
+        StringToWordVector filter = new StringToWordVector(1000);
+        WordTokenizer customTokenizer = new WordTokenizer();
+        customTokenizer.setDelimiters(" \t\n.,;:'\"()?!+");
 
-		Evaluation eval = new Evaluation(instances);
-		eval.crossValidateModel(classifier, instances, 10, new Random());
-	}
+        filter.setLowerCaseTokens(true);
+        filter.IDFTransformTipText();
+        filter.setStopwordsHandler(new Rainbow());
+        filter.setStemmer(new SnowballStemmer());
+        filter.setTokenizer(customTokenizer);
+        filter.setInputFormat(instances);
+
+        Instances vectorInstances = Filter.useFilter(instances, filter);
+
+        // numeric to nominal data
+        NumericToNominal ntn = new NumericToNominal();
+        ntn.setAttributeIndices("first");
+        ntn.setInputFormat(vectorInstances);
+
+        Instances nominalInstances = Filter.useFilter(vectorInstances, ntn);
+
+        // train classifier
+        NaiveBayes classifier = new NaiveBayes();
+        classifier.buildClassifier(nominalInstances);
+
+        // evaluation
+        Evaluation eval = new Evaluation(nominalInstances);
+        eval.crossValidateModel(classifier, nominalInstances, 10, new Random());
+
+        System.out.println(eval.toClassDetailsString());
+        System.out.println(eval.toSummaryString());
+    }
 }
