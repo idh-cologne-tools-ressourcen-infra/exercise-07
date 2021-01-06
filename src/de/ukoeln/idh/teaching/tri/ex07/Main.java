da7e6013a1bc6252374213a6163ea251df04b94a
davidwegmann <64281764+davidwegmann@users.noreply.github.com>
2021-01-06 23:26:30 +0100

    Main.java geändert
    Added train classifier
    //filter - versucht..

--- src/de/ukoeln/idh/teaching/tri/ex07/Main.java
+++ src/de/ukoeln/idh/teaching/tri/ex07/Main.java
@@ -1,13 +1,18 @@
 package de.ukoeln.idh.teaching.tri.ex07;
 
 import java.io.File;
+
 import java.io.IOException;
 import java.util.Random;
 
 import weka.classifiers.Evaluation;
-import weka.classifiers.rules.OneR;
+import weka.classifiers.trees.RandomForest;
 import weka.core.Instances;
 import weka.core.converters.ArffLoader;
+import weka.filters.Filter;
+import weka.filters.unsupervised.attribute.StringToWordVector;
+import weka.filters.unsupervised.attribute.NumericToNominal;
+
 
 public class Main {
 	public static void main(String[] args) throws IOException, Exception {
@@ -19,10 +24,12 @@ public class Main {
 		instances.setClassIndex(instances.numAttributes() - 1);
 
 		// train classifier
-		OneR classifier = new OneR();
+		RandomForest classifier = new RandomForest();
 		classifier.buildClassifier(instances);
 
 		Evaluation eval = new Evaluation(instances);
 		eval.crossValidateModel(classifier, instances, 10, new Random());
-	}
+	
+		//filter...
+	}	
 }
