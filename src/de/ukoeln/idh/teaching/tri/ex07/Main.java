a0ec703d807d275128e8dc009bd8c0b8a420a5bc
jkoch26-create <74219021+jkoch26-create@users.noreply.github.com>
2021-01-06 18:21:18 +0100

    Not Working

--- src/de/ukoeln/idh/teaching/tri/ex07/Main.java
+++ src/de/ukoeln/idh/teaching/tri/ex07/Main.java
@@ -1,13 +1,18 @@
 package de.ukoeln.idh.teaching.tri.ex07;
 
 import java.io.File;
+
 import java.io.IOException;
 import java.util.Random;
 
 import weka.classifiers.Evaluation;
-import weka.classifiers.rules.OneR;
+import weka.classifiers.trees.RandomForest;
 import weka.core.Instances;
 import weka.core.converters.ArffLoader;
+import weka.filters.Filter;
+import weka.filters.unsupervised.attribute.StringToWordVector;
+import weka.filters.unsupervised.attribute.NumericToNominal;
+
 
 public class Main {
 	public static void main(String[] args) throws IOException, Exception {
@@ -19,10 +24,25 @@ public class Main {
 		instances.setClassIndex(instances.numAttributes() - 1);
 
 		// train classifier
-		OneR classifier = new OneR();
+		RandomForest classifier = new RandomForest();
 		classifier.buildClassifier(instances);
 
 		Evaluation eval = new Evaluation(instances);
 		eval.crossValidateModel(classifier, instances, 10, new Random());
-	}
+	
+		//filter
+		StringToWordVector filter = new StringToWordVector(1000);
+		filter.attributeIndicesTipText();
+		
+		Instances filteredSet = Filter.useFilter(instances, filter);
+	
+		NumericToNominal ntnFilter = new NumericToNominal();
+		ntnFilter.setAttributeIndices("first");
+		ntnFilter.setInputFormat(filteredSet);
+		
+		Instances filteredSet1 = Filter.useFilter(filteredSet, ntnFilter);
+		
+		RandomForest classifiers = new RandomForest();
+		classifiers.buildClassifier(filteredSet1);
+	}	
 }
