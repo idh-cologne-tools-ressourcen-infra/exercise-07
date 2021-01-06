abd5f6db47a6300767feed6064e0bfcbcf37b691
Al <nalcock1@smail.uni-koeln.de>
2021-01-06 20:29:40 +0100

    Access Error

--- src/de/ukoeln/idh/teaching/tri/ex07/Main.java
+++ src/de/ukoeln/idh/teaching/tri/ex07/Main.java
@@ -5,24 +5,26 @@ import java.io.IOException;
 import java.util.Random;
 
 import weka.classifiers.Evaluation;
+import weka.classifiers.bayes.NaiveBayes;
 import weka.classifiers.rules.OneR;
 import weka.core.Instances;
 import weka.core.converters.ArffLoader;
 
-public class Main {
-	public static void main(String[] args) throws IOException, Exception {
+	public class Main {
+		public static void main(String[] args) throws IOException, Exception {
 
-		// load data
-		ArffLoader loader = new ArffLoader();
-		loader.setFile(new File("data.arff"));
-		Instances instances = loader.getDataSet();
-		instances.setClassIndex(instances.numAttributes() - 1);
+			// load data
+			ArffLoader loader = new ArffLoader();
+			loader.setFile(new File("imdb.arff"));
+			Instances instances = loader.getDataSet();
+			instances.setClassIndex(instances.numAttributes() - 1);
 
-		// train classifier
-		OneR classifier = new OneR();
-		classifier.buildClassifier(instances);
+			// train classifier
+			NaiveBayes classifier = new NaiveBayes();
+			classifier.buildClassifier(instances);
 
-		Evaluation eval = new Evaluation(instances);
-		eval.crossValidateModel(classifier, instances, 10, new Random());
-	}
+			Evaluation eval = new Evaluation(instances);
+			eval.crossValidateModel(classifier, instances, 10, new Random());
+		}
 }
+
