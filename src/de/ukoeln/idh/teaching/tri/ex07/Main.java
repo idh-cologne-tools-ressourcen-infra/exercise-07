2f9d686f8338b4c691aa0cd32f8f93e269f8b608
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2021-01-07 10:11:06 +0000

    reference solution

--- src/de/ukoeln/idh/teaching/tri/ex07/Main.java
+++ src/de/ukoeln/idh/teaching/tri/ex07/Main.java
@@ -5,24 +5,52 @@ import java.io.IOException;
 import java.util.Random;
 
 import weka.classifiers.Evaluation;
-import weka.classifiers.rules.OneR;
+import weka.classifiers.bayes.NaiveBayes;
 import weka.core.Instances;
 import weka.core.converters.ArffLoader;
+import weka.core.tokenizers.WordTokenizer;
+import weka.filters.Filter;
+import weka.filters.unsupervised.attribute.NumericToNominal;
+import weka.filters.unsupervised.attribute.StringToWordVector;
 
 public class Main {
 	public static void main(String[] args) throws IOException, Exception {
 
 		// load data
 		ArffLoader loader = new ArffLoader();
-		loader.setFile(new File("data.arff"));
+		loader.setFile(new File("data/imdb.arff"));
 		Instances instances = loader.getDataSet();
 		instances.setClassIndex(instances.numAttributes() - 1);
 
+		// apply filter 1: Convert strings to word vectors
+		WordTokenizer wtokenizer = new WordTokenizer();
+		wtokenizer.setDelimiters("\t \n\r<>?:,.{})([]");
+
+		StringToWordVector filter = new StringToWordVector();
+		filter.setAttributeIndices("1");
+		filter.setLowerCaseTokens(true);
+		filter.setTokenizer(wtokenizer);
+		filter.setMinTermFreq(10);
+		filter.setInputFormat(instances);
+
+		// run filter
+		instances = Filter.useFilter(instances, filter);
+
+		// apply filter 2: Convert target attribute from numeric to nominal
+		NumericToNominal n2n = new NumericToNominal();
+		n2n.setAttributeIndices("1");
+		n2n.setInputFormat(instances);
+
+		// run filter
+		instances = Filter.useFilter(instances, n2n);
+
 		// train classifier
-		OneR classifier = new OneR();
+		NaiveBayes classifier = new NaiveBayes();
 		classifier.buildClassifier(instances);
 
+		// Run evaluation
 		Evaluation eval = new Evaluation(instances);
 		eval.crossValidateModel(classifier, instances, 10, new Random());
+		System.out.println(eval.toSummaryString());
 	}
 }
