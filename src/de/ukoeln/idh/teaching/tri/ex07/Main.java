d5927b59f869c3b352c41095bf7665d2be298cae
jrein1 <jreinha8@smail.uni-koeln.de>
2021-01-08 15:00:24 +0100

    Update Main.java

--- src/de/ukoeln/idh/teaching/tri/ex07/Main.java
+++ src/de/ukoeln/idh/teaching/tri/ex07/Main.java
@@ -3,13 +3,13 @@ package de.ukoeln.idh.teaching.tri.ex07;
 import java.io.File;
 import java.io.IOException;
 import java.util.Random;
+
 import weka.classifiers.Evaluation;
-import weka.*;
-import weka.classifiers.rules.OneR;
+import weka.classifiers.bayes.NaiveBayes;
 import weka.core.Instances;
 import weka.core.converters.ArffLoader;
-import weka.core.converters.ConverterUtils.*;
-import weka.core.converters.ConverterUtils.DataSource;
+import weka.core.tokenizers.WordTokenizer;
+import weka.core.tokenizers.Tokenizer;
 import weka.filters.Filter;
 import weka.filters.unsupervised.attribute.NumericToNominal;
 import weka.filters.unsupervised.attribute.StringToWordVector;
@@ -22,26 +22,37 @@ public class Main {
 		loader.setFile(new File("/exercise-07/data/imdb.arff"));
 		Instances data = loader.getDataSet();
 		data.setClassIndex(data.numAttributes() - 1);
-		data.setClassIndex(1);
 		
+		//Filter String to Word
+		WordTokenizer wtokenizer = new WordTokenizer();
+		wtokenizer.setDelimiters("\t \n\r<>?:,.{})([]");
+		
+		StringToWordVector stToWo = new StringToWordVector();
+		stToWo.setAttributeIndices("1");
+		stToWo.setLowerCaseTokens(true);
+		stToWo.setTokenizer(wtokenizer);
+		stToWo.setMinTermFreq(10);
+		stToWo.setInputFormat(data);
+		
+		//Filter ausführen
+		data = Filter.useFilter(data, stToWo);
+		
+		//Filter Numeric toNominal
 		NumericToNominal nuToNo = new NumericToNominal();
+		nuToNo.setAttributeIndices("1");
 		nuToNo.setInputFormat(data);
-		Instances filtered = Filter.useFilter(data, nuToNo);
-		System.out.println(filtered);
 		
-		StringToWordVector stToWo = new StringToWordVector(1000);
-		stToWo.setInputFormat(filtered);
-		Instances filtered2 = Filter.useFilter(filtered, stToWo);
+		//ausführen NtN
+		data = Filter.useFilter(data, nuToNo);
 		
-		System.out.println(filtered2);
-		
-		System.out.println();
 		// train classifier
-		OneR classifier = new OneR();
+		NaiveBayes classifier = new NaiveBayes();
 		classifier.buildClassifier(data);
-
+		
+		//run eval
 		Evaluation eval = new Evaluation(data);
 		eval.crossValidateModel(classifier, data, 10, new Random());
+		System.out.println(eval.toSummaryString());
 
 	}
 }
