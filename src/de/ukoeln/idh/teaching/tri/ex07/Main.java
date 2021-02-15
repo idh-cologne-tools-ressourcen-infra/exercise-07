b1b3a260d9197966dee24a8ba3d889ff4c8abc50
kekpja <56304716+kekpja@users.noreply.github.com>
2021-02-15 14:23:03 +0100

    NaiveBayes + weka.jar

--- src/de/ukoeln/idh/teaching/tri/ex07/Main.java
+++ src/de/ukoeln/idh/teaching/tri/ex07/Main.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.util.Random;
 
 import weka.classifiers.Evaluation;
-import weka.classifiers.rules.OneR;
+import weka.classifiers.bayes.NaiveBayes;
 import weka.core.Instances;
 import weka.core.converters.ArffLoader;
 
@@ -14,14 +14,15 @@ public class Main {
 
 		// load data
 		ArffLoader loader = new ArffLoader();
-		loader.setFile(new File("data.arff"));
+		loader.setFile(new File("imdb.arff"));
 		Instances instances = loader.getDataSet();
 		instances.setClassIndex(instances.numAttributes() - 1);
 
 		// train classifier
-		OneR classifier = new OneR();
+		NaiveBayes classifier = new NaiveBayes();
 		classifier.buildClassifier(instances);
 
+		//
 		Evaluation eval = new Evaluation(instances);
 		eval.crossValidateModel(classifier, instances, 10, new Random());
 	}
